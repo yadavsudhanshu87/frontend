10cca18b8ec755a1ff0f2fbb6218a8bedb1213c5
SUDHANSHU YADAV <50865330+yadavsudhanshu87@users.noreply.github.com>
2019-05-29 13:49:48 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,12 @@
+simple_array=[1,4,7,"Hello","World"];
+
+console.log(simple_array);
+var l= simple_array.length
+for(var i=0;i<l;i++)
+{
+	 console.log(simple_array[i])
+}
+
+console.log(simple_array.pop());
+
+console.log(simple_array.push(200));
